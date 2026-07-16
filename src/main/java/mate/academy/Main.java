93431c2baeb661678197457fa2952ddb2df5c8ab
Den <99994444@ukr.net>
2026-07-16 19:38:18 +0300

    for check

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Terminator 4");
+        movie.setDescription("Awesome movie");
+
+        Movie addedMovie = movieService.add(movie);
+
+        Movie movieFromDb = movieService.get(addedMovie.getId());
+        System.out.println("Movie from DB: " + movieFromDb);
     }
 }
